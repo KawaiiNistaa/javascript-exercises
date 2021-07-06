30d936f6d9b03fb19f0fb3b4087a0102c437aaaf
Isabella Thurmond <ithurmond@shutterstockgit.com>
2021-07-06 14:32:15 -0700

    completed reverse string
    completed sum all
    
    added extra test case for sum all

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,24 @@
-const reverseString = function() {
+const reverseString = function(string) {
+ 
+    let empty = "";
+
+    
+    for (let i = string.length - 1; i >= string.length-string.length; i--){
+        empty = empty + string[i];
+
+    }
+    console.log(empty);
+    return empty;
+
 
 };
 
 module.exports = reverseString;
+
+
+// we have a string
+// what i need to do is: have an empty string
+// find out the char length of the string
+// make a for loop and reverse it : i needs to be equal to the full length of the string
+//we need to get to the start number which is 0 and make the loop end on that number
+// we keep adding each letter in each for loop iteration to get the fully reversed word
--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,38 @@
-const sumAll = function() {
+const sumAll = function(begin, end) {
+
+    let sum = 0;
+    let wrong = 'ERROR';
+
+    if (begin < 0) {
+        return wrong;
+    } else if (typeof end != "number" || typeof begin != "number") {
+        return wrong;
+
+    }
+
+
+
+
+    if (end > begin){
+        for (let i = begin; i <= end; i++){
+            sum = sum + i;
+        }
+    } else if ( begin > end) {
+        for (let i = end; i <= begin; i++){
+            sum = sum + i;
+        }
+    }   
+    return sum;
+    
+
 
 };
 
 module.exports = sumAll;
+
+
+// ok so i am kinda unsure so ill just psedo code it
+// for loop with parameters of 1st to last number
+// empty variable
+//add to the variable like var = var + i so i wont run into any reset issue with my loop
+//print loop
